38906b21096d7c2bd32bf0bc709b13e04a0c96f3
vaddisanjeevkumar9676@gmail.com <vaddisanjeevkumar9676@gmail.com>
2022-11-12 21:05:13 +0530

    E-commrece Website Project

---
+++ E-commerce website/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91-9701041108 </p>
+         <p> <i class="fas fa-phone"></i> +91-8500853841 </p>
+         <p> <i class="fas fa-envelope"></i> Sanjeev_Buddy_Shopi@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> A.P, india - 518962 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="www.facebook.com"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="www.twitter.com"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="www.instagram.com"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span> Sanjeev_Buddy_Shopi </span> </p>
+
+</section>
