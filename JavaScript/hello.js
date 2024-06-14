12488381868a276a68fbd709e766427e65ddd0c5
V_SANJEEV_KUMAR <72122950+vskumar9@users.noreply.github.com>
2024-06-14 06:31:47 +0000

    functions and arrays

--- JavaScript/hello.js
+++ JavaScript/hello.js
@@ -1,1 +1,54 @@
 console.log("Hello world");
+// ------------------------------------------------
+console.log(eval("3+5"));
+// ------------------------------------------------
+(function() {
+        "use strict";
+        function greetMe(Name){
+            console.log(`Hello ${Name}`);
+        }
+
+        greetMe("Sanjeev");
+    })();
+
+// ------------------------------------------------
+
+if(Math.random() > 0.5) {
+    const y = 5; // ReferenceError: y is not defined
+}
+// console.log(y);
+
+// ------------------------------------------------
+
+if(true){
+    var x = 10;
+}
+
+console.log(x);
+
+// ------------------------------------------------
+
+console.log(x === undefined); // True
+
+var x = 3;
+
+(function(){
+    var x;
+    console.log(x); // undefined
+    x = "local value";
+})();
+
+// -----------------------------------------
+
+const MY_OBJECT = { key: "value" };
+MY_OBJECT.key = "otherValue";
+
+console.log(MY_OBJECT); // { key: 'otherValue' }
+
+// -------------------------------------------
+
+const MY_ARRAY = ["HTML", "CSS"];
+MY_ARRAY.push("JAVASCRIPT");
+console.log(MY_ARRAY); // ['HTML', 'CSS', 'JAVASCRIPT'];
+var name = MY_ARRAY.pop();
+console.log(name);
