3a277202c54e77690fb3abeb79d6230c40f921d7
V_SANJEEV_KUMAR <72122950+vskumar9@users.noreply.github.com>
2024-06-20 12:29:11 +0000

    update

---
+++ JavaScript/Operators.js
@@ -0,0 +1,36 @@
+/**
+ * Arithmetic
+ * = Addition(+)
+ * = Subtraction(-)
+ * = Multiplication(*)
+ * = Division(/)
+ * = Modulus(%)
+ * = Exponentiation(**)
+ * comparison
+ * = Equal to(=)
+ * = Not equal to(!=)
+ * = Strict eqaul to(===)
+ * = Strict not equal to(!==)
+ * = Greater than(>)
+ * = Greater than or equal to(>=)
+ * = Less than(<)
+ * = Less than or equal to(<=)
+ */
+
+let a = 10, b = 5;
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+console.log(a, b)
+console.log(a==b)
+console.log(a===b)
+console.log(a!=b)
+console.log(a!==b)
+console.log(a>b)
+console.log(a>=b)
+console.log(a<b)
+console.log(a<=b)
