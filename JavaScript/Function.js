bf4e3793a2c4ec21e142b0207954bceddf4af23d
V_SANJEEV_KUMAR <72122950+vskumar9@users.noreply.github.com>
2024-07-09 08:09:58 +0000

    update

---
+++ JavaScript/Function.js
@@ -0,0 +1,16 @@
+/**
+ * Let's get started. Functions are a fundamental part of JavaScript. 
+ * A function is a block of code that performs a specific task. You 
+ * can use functions to organize your code into reusable pieces that 
+ * can be called from different parts of the program. In JavaScript, 
+ * you can define functions using the function keyword. Let's look at 
+ * the syntax of a function. We have a keyword function, that is used 
+ * to define a function in JavaScript,
+ */
+
+function addNumbers(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+console.log(addNumbers(10, 20));
